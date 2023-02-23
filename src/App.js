b6efef3023713c8ae1488898bcc45bf955462f65
yujin000915 <yujin3717@naver.com>
2023-02-23 14:04:30 +0900

    프로젝트 생성

--- src/App.js
+++ src/App.js
@@ -2,24 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+		<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<p>
+				리액트 스터디 2조 : 박유진, 장원미, 신균욱, 최원석
+			</p>
+			<a
+			className="App-link"
+			href="https://reactjs.org"
+			target="_blank"
+			rel="noopener noreferrer"
+			>
+			Learn React
+			</a>
+		</header>
+		</div>
+	);
 }
 
 export default App;
