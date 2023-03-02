caafe5b4dd261b70de9c17916128541eb7635778
최원석 <cws4987@siwonschool.com>
2023-03-02 16:03:45 +0900

    글쓰기

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { withRouter, useHistory } from 'react-router-dom';
 import { changeField, initializeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
 import { check } from '../../modules/user';
